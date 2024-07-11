c163c23ffa98e2338fcb6adac162325cd4cb4ae1
DiegoWEBD <diegomz.webdev@gmail.com>
2024-07-10 20:39:30 -0400

    security configurations

--- src/infrastructure/database/database.postgres.ts
+++ src/infrastructure/database/database.postgres.ts
@@ -18,7 +18,7 @@ export class PgDatabase implements Database {
 			host: process.env.DB_HOST,
 			port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
 			database: process.env.DB_NAME,
-			user: process.env.DB_USERNAME,
+			user: process.env.DB_USER,
 			password: process.env.DB_PASSWORD,
 		})
 		this.isConnected = true
